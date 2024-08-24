58d5185227f33e5d70d51b33b7d21397c5179e90
duc-minh-droid <ducminhcsp@gmail.com>
2024-08-24 20:01:40 +0100

    nice

--- retrieve_commits/manage_repos.py
+++ retrieve_commits/manage_repos.py
@@ -1,17 +1,12 @@
 import os
 import subprocess
 
-# List of targeted repositories
-targeted_repos = [
-    'https://github.com/openai/gpt-3.git'
-]
-
 def run_script_in_repo(repo_url):
     script_path = 'retrieve_commits/collect_commit_data.py'
     result = subprocess.run(['python3', script_path, repo_url], capture_output=True, text=True)
     print(f"Script output for {repo_url}: {result.stdout}")
 
-def main():
+def main(targeted_repos):
     for repo in targeted_repos:
         # if os.path.exists(os.path.join(repo, '.git')):
         print(f"Running script in {repo}")
@@ -20,4 +15,8 @@ def main():
         #     print(f"{repo} is not a valid Git repository")
 
 if __name__ == "__main__":
-    main()
+    # List of targeted repositories
+    targeted_repos = [
+        'https://github.com/openai/gpt-3.git'
+    ]
+    main(targeted_repos)
